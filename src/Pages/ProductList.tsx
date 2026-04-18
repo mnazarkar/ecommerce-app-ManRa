e4d05a6ce663c7281e757bb78c4db55af1d3133e
Asphalt 2.0 <ssnazarkar11@gmail.com>
2026-04-19 00:06:03 +0530

    Added PLP PDP Pages and action reducers for it

--- src/Pages/ProductList.tsx
+++ src/Pages/ProductList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../actions/productActions";
