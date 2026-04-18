9dd546bb2f785046cb2fd677d4cbba840f7a526d
Asphalt 2.0 <ssnazarkar11@gmail.com>
2026-04-18 21:48:46 +0530

    removed unused imports

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ShoppingCart, MapPin, User } from "lucide-react";
+import { ShoppingCart, User } from "lucide-react";
 import SearchBar from "./SearchBar";
 import { useSelector } from "react-redux";
 
