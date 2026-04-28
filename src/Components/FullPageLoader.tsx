ccb8e7ceb30d150154da0ae54f522210e9bfa359
Asphalt 2.0 <ssnazarkar11@gmail.com>
2026-04-28 17:35:43 +0530

    added code for colors for loader

--- src/Components/FullPageLoader.tsx
+++ src/Components/FullPageLoader.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 function FullPageLoader() {
   return (
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-100">
-      <Loader2 className="w-12 h-12 animate-spin text-white" />
+      <Loader2 className="w-12 h-12 animate-spin text-fuchsia-500" />
     </div>
   );
 }
