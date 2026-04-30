a87c22de2f20feff5d61496efddfc06c9b7d7f3a
Asphalt 2.0 <ssnazarkar11@gmail.com>
2026-04-30 17:39:55 +0530

    added code for cartslice and add to cart feature

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit"
-import { useDispatch } from "react-redux";
 
 type Product = {
     id: number;
